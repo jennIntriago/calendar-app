04411ad44057442214b02d45785cc6fd16a17f30
Jennifer Intriago <jenniferintriago28@outlook.es>
2023-02-06 20:14:58 -0500

    escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,6 +3,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { addHours } from "date-fns";
 import { CalendarEvent, Navbar } from "../";
 import { localizer, getMessagesES } from "../../helpers";
+import { useState } from "react";
 
 const events = [
   {
@@ -18,6 +19,9 @@ const events = [
   },
 ];
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "week"
+  );
   const eventStyleGetter = (event, start, end, isSeleted) => {
     // console.log({ event, start, end, isSeleted });
     const style = {
@@ -28,6 +32,20 @@ export const CalendarPage = () => {
     };
     return { style };
   };
+
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event });
+  };
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+  };
+  const onViewChanged = (event) => {
+    console.log({ viewChange: event });
+    //se guardara en el localstorage para asi recordarlos
+    localStorage.setItem("lastView", event);
+    setLastView(event);
+  };
   return (
     <>
       <Navbar />
@@ -36,6 +54,7 @@ export const CalendarPage = () => {
         culture="es"
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: "calc(100vh - 80px)" }}
@@ -44,6 +63,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent,
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   );
